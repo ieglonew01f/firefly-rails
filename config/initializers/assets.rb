9484f5545b41c63397c91eb563e98795e756cdad
Mukunda Gogoi <ieglonew01f@gmail.com>
2016-03-11 02:33:56 +0530

    trying to fix assets damnit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( index/firefly.index.js index/firefly.index.css)
+Rails.application.config.assets.precompile += %w( line-icons/simple-line-icons.css )
+Rails.application.config.assets.precompile += %w( index/firefly.index.js index/firefly.index.css )
 Rails.application.config.assets.precompile += %w( helpers/posts/firefly.posts.js )
-Rails.application.config.assets.precompile += %w( home/firefly.home.js home/firefly.home.css)
+Rails.application.config.assets.precompile += %w( home/firefly.home.js home/firefly.home.css )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
